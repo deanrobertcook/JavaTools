2e951ab23a50bbd8eead0c91ac7a2f6bdf9e5df7
deanrobertcook <deanrobertcook@gmail.com>
2015-02-21 21:15:16 +0100

    Implemented simple HashTable - currently no collision handling

---
+++ src/datastructures/HashTable.java
@@ -0,0 +1,42 @@
+package datastructures;
+
+import utility.Utility;
+
+/**
+ *
+ * @author dean
+ */
+public class HashTable {
+	
+	private String[] backingArray;
+	
+	public HashTable() {
+		this(10);
+	}
+	
+	public HashTable(int initialSize) {
+		this.backingArray = new String[initialSize];
+	}
+	
+	public void insert(String index, String value) {
+		int hash = this.sumCharValueHash(index);
+		this.backingArray[hash] = value;
+	}
+	
+	private int sumCharValueHash(String index) {
+		char[] indexChars = index.toCharArray();
+		int hash = 0;
+		for (int i = 0; i < indexChars.length; i++) {
+			hash += (int)indexChars[i];
+		}
+		hash = Utility.pow(hash, 2);
+		hash = hash % this.backingArray.length;
+		return hash;
+	}
+	
+	public String get(String index) {
+		int hash = this.sumCharValueHash(index);
+		String value = this.backingArray[hash];
+		return value;
+	}
+}
---
+++ test/datastructures/HashTableTest.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datastructures;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import utility.TestCase;
+
+/**
+ *
+ * @author dean
+ */
+public class HashTableTest extends TestCase {
+	
+	@Test
+	public void testSimpleInsertAndGet() {
+		HashTable hashTable = new HashTable();
+		String index = "index";
+		String value = "value";
+		
+		hashTable.insert(index, value);
+		
+		assertEquals(value, hashTable.get(index));
+	}
+	
+	@Test
+	public void testMultipleInsertsAndGets() {
+		String[] indices = {
+			"index1",
+			"index2",
+			"index3",
+		};
+		String[] values = {
+			"value1",
+			"value2",
+			"value3",
+		};
+		
+		HashTable hashTable = new HashTable();
+		for (int i = 0; i < indices.length; i++) {
+			hashTable.insert(indices[i], values[i]);
+		}
+		for (int i = 0; i < indices.length; i++) {
+			assertEquals(values[i], hashTable.get(indices[i]));
+		}
+	}
+	
+}
