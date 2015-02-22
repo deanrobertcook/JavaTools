015229b5504d1ed46313a7db4d3f9bd8b1d43ac9
deanrobertcook <deanrobertcook@gmail.com>
2015-02-22 10:37:12 +0100

    Updated findMin and Max to handle 0 length arrays

--- src/utility/Utility.java
+++ src/utility/Utility.java
@@ -10,58 +10,65 @@ package utility;
  * @author dean
  */
 public class Utility {
+
 	public static void printArray(int[] A) {
 		System.out.println("Array Size: " + A.length);
 		System.out.printf("%-10s%s\n", "Index", "Value");
-		for(int i = 0; i < A.length; i++) {
+		for (int i = 0; i < A.length; i++) {
 			System.out.printf("%-10s %d;\n", "A[" + i + "] = ", A[i]);
 		}
 		System.out.println("------------------------------------");
 	}
-	
+
 	public static void printArray(String[] A) {
 		System.out.println("Array Size: " + A.length);
 		System.out.printf("%-10s%s\n", "Index", "Value");
-		for(int i = 0; i < A.length; i++) {
+		for (int i = 0; i < A.length; i++) {
 			System.out.printf("%-10s %s;\n", "A[" + i + "] = ", A[i]);
 		}
 		System.out.println("------------------------------------");
 	}
-	
+
 	public static int findMinimum(int[] A) {
-		int minPos = 0;
-		int lowestVal = A[0];
-		for (int i = 1; i < A.length; i++) {
-			int valAtCurrent = A[i];
-			if (valAtCurrent < lowestVal) {
-				minPos = i;
-				lowestVal = A[i];
+		if (A.length > 0) {
+			int minPos = 0;
+			int lowestVal = A[0];
+			for (int i = 1; i < A.length; i++) {
+				int valAtCurrent = A[i];
+				if (valAtCurrent < lowestVal) {
+					minPos = i;
+					lowestVal = A[i];
+				}
 			}
+			return minPos;
 		}
-		return minPos;
+		return -1;
 	}
-	
+
 	public static int findMaximum(int[] A) {
-		int maxPos = 0;
-		int highestVal = A[0];
-		for (int i = 1; i < A.length; i++) {
-			int valAtCurrent = A[i];
-			if (valAtCurrent > highestVal) {
-				maxPos = i;
-				highestVal = A[i];
+		if (A.length > 0) {
+			int maxPos = 0;
+			int highestVal = A[0];
+			for (int i = 1; i < A.length; i++) {
+				int valAtCurrent = A[i];
+				if (valAtCurrent > highestVal) {
+					maxPos = i;
+					highestVal = A[i];
+				}
 			}
+			return maxPos;
 		}
-		return maxPos;
+		return -1;
 	}
-	
+
 	public static int sumOfArray(int[] A) {
 		int sum = 0;
-		for(int i = 0; i < A.length; i++) {
+		for (int i = 0; i < A.length; i++) {
 			sum += A[i];
 		}
 		return sum;
 	}
-	
+
 	public static int sumOfArraySlice(int[] A, int start, int end) {
 		int sum = 0;
 		for (int i = start; i < end; i++) {
@@ -69,29 +76,31 @@ public class Utility {
 		}
 		return sum;
 	}
-	
+
 	public static int sumOfIntegers(int integer) {
 		int sum = 0;
-		for(int i = 1; i <= integer; i++) {
+		for (int i = 1; i <= integer; i++) {
 			sum += i;
 		}
 		return sum;
 	}
-	
-	public static double avgOfArray(int[] A ) {
+
+	public static double avgOfArray(int[] A) {
 		int sum = 0;
-		for (int d : A ) sum += d;
+		for (int d : A) {
+			sum += d;
+		}
 		double avg = (1.0d * sum) / A.length;
 		return avg;
 	}
-	
+
 	public static int findSigFigs(int n) {
 		int placeValue = 0;
-		
+
 		if (n == 0) {
 			return 0;
 		}
-		
+
 		while (n / 10 >= 1) {
 			placeValue++;
 			n = n / 10;
@@ -99,7 +108,7 @@ public class Utility {
 		placeValue++;
 		return placeValue;
 	}
-	
+
 	public static int[] findIntValuesOfStringCharacters(String string) {
 		char[] indexAsArray = string.toCharArray();
 		int[] asciiArray = new int[indexAsArray.length];
@@ -109,16 +118,17 @@ public class Utility {
 		}
 		return asciiArray;
 	}
-	
+
 	/**
 	 * Wrapper function for Math.pow() for working only with integers. Saves
 	 * tedious casting.
+	 *
 	 * @param base
 	 * @param exponent
-	 * @return 
+	 * @return
 	 */
 	public static int pow(int base, int exponent) {
 		return (int) Math.pow((double) base, (double) exponent);
 	}
-	
+
 }
--- test/utility/UtilityTest.java
+++ test/utility/UtilityTest.java
@@ -10,48 +10,48 @@ import static org.junit.Assert.*;
  * @author dean
  */
 public class UtilityTest extends TestMethods {
+
 	@Test
 	public void testFindPlaceValue() {
 		System.out.println("findPlaceValue");
 		int[] testInputs = {
-			10000,	8964,	5,	717322,	0
+			10000, 8964, 5, 717322, 0
 		};
 		int[] expectedOutputs = {
-			5,		4,		1,	6,		0
+			5, 4, 1, 6, 0
 		};
-		
+
 		for (int i = 0; i < testInputs.length; i++) {
 			assertEquals(expectedOutputs[i], Utility.findSigFigs(testInputs[i]));
 		}
 	}
-	
+
 	@Test
 	public void testFindIntValuesOfStringCharacters() {
 		String testString = "abcdABCD1234!@#$%^&*()";
 		int[] expectedVals = {
-			97, 98, 99, 100, 
-			65, 66, 67, 68, 
+			97, 98, 99, 100,
+			65, 66, 67, 68,
 			49, 50, 51, 52,
 			33, 64, 35, 36, 37, 94, 38, 42, 40, 41
 		};
 		int[] intVals = Utility.findIntValuesOfStringCharacters(testString);
 		assertArrayEquals(expectedVals, intVals);
 	}
-	
+
 	@Test
 	public void testFindMinimumAndMax() {
-		int[][] arrays = this.randomIntegerArrays(1000, 100, 10);		
+		int[][] arrays = this.randomIntegerArrays(1000, 100, 10);
 		for (int[] array : arrays) {
 			int testMin = Utility.findMinimum(array);
 			int valueAtMin = array[testMin];
 			int testMax = Utility.findMaximum(array);
 			int valueAtMax = array[testMax];
-			
+
 			Arrays.sort(array);
 			assertEquals(array[0], valueAtMin);
 			assertEquals(array[array.length - 1], valueAtMax);
 		}
-		
+
 	}
-	
 }
