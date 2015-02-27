89e1a5cbc7d311fdaf3249d580a6f139ca66021e
deanrobertcook <deanrobertcook@gmail.com>
2015-02-27 11:40:23 +0100

    Placed limit on number of client connections allowed

--- src/network/Server.java
+++ src/network/Server.java
@@ -2,11 +2,83 @@ package network;
 
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
+import utility.Util;
 
 /**
  * @author Dean Cook<deanrobertcook@gmail.com>
  */
 public class Server {
+
+	private int portNumber;
+	private final ServerSocket serverSocket;
+	private boolean listening = true;
+	private final int maxThreads = 2;
+	private boolean[] threadIds = new boolean[this.maxThreads];
+
+	public Server(int portNumber) {
+		this.portNumber = portNumber;
+		ServerSocket socket = null;
+		try {
+			socket = new ServerSocket(this.portNumber);
+		} catch (IOException e) {
+			System.err.println("Could not listen on port " + portNumber);
+			System.exit(-1);
+		}
+		this.serverSocket = socket;
+		
+		for (int i = 0; i < this.threadIds.length; i++) {
+			threadIds[i] = false;
+		}
+	}
+
+	public void listen() {
+		while (listening) {
+			try {
+				Socket socket = this.serverSocket.accept();
+				int nextId = this.nextThreadId();
+				ServerThread serverThread = new ServerThread(
+						this, socket, nextId);
+				this.addConnection(serverThread, nextId);
+			} catch (IOException e) {
+				System.err.println(e.getMessage());
+			}
+		}
+	}
+
+	public void addConnection(ServerThread serverThread, int nextId) {
+		System.out.println("Attempting to connect to client: " + nextId);
+		if (nextId < this.maxThreads) {
+			this.threadIds[nextId] = true;
+			new Thread(serverThread).start();
+		} else {
+			serverThread.closeConnection();
+		}
+		Util.printArray(threadIds, "Threads free: ");
+		System.out.println("Next free thread: " + this.nextThreadId());
+	}
+	
+	public void removeConnection(int threadId) {
+		System.out.println("Closing connection to client: " + threadId);
+		if (threadId < this.maxThreads) { //if the socket was a full connection
+			this.threadIds[threadId] = false;
+		}
+		Util.printArray(threadIds, "Threads free: ");
+		System.out.println("Next free thread: " + this.nextThreadId());
+	}
+	
+	public int nextThreadId() {
+		for (int i = 0; i < this.threadIds.length; i++) {
+			if (!this.threadIds[i]) {
+				return i;
+			}
+		}
+		return this.threadIds.length;
+	}
+
+	public void stopListening() {
+		this.listening = false;
+	}
 
 	public static void main(String[] args) throws IOException {
 
@@ -16,15 +88,6 @@ public class Server {
 		}
 
 		int portNumber = Integer.parseInt(args[0]);
-		boolean listening = true;
-
-		try (ServerSocket serverSocket = new ServerSocket(portNumber)) {
-			while (listening) {
-				new ServerThread(serverSocket.accept()).start();
-			}
-		} catch (IOException e) {
-			System.err.println("Could not listen on port " + portNumber);
-			System.exit(-1);
-		}
+		new Server(portNumber).listen();
 	}
 }
--- src/network/ServerThread.java
+++ src/network/ServerThread.java
@@ -9,29 +9,57 @@ import java.net.Socket;
 /**
  * @author Dean Cook<deanrobertcook@gmail.com>
  */
-public class ServerThread extends Thread {
-	
-	private final Socket clientSocket;
-	
-	public ServerThread(Socket clientSocket) {
-		super("Server");
-		this.clientSocket = clientSocket;	
+public class ServerThread implements Runnable {
+
+	protected final Server server;
+	protected final Socket clientSocket;
+	protected final int threadId;
+
+	public ServerThread(Server server, Socket clientSocket, int threadId) {
+		this.server = server;
+		this.clientSocket = clientSocket;
+		this.threadId = threadId;
 	}
-	
+
+	public PrintWriter getOutStream() {
+		PrintWriter out = null;
+		try {
+			out = new PrintWriter(this.clientSocket.getOutputStream(), true);
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+		return out;
+	}
+
+	public BufferedReader getInStream() {
+		BufferedReader in = null;
+		try {
+			in = new BufferedReader(
+					new InputStreamReader(this.clientSocket.getInputStream()));
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		return in;
+	}
+
 	@Override
 	public void run() {
+		PrintWriter out = this.getOutStream();
+		BufferedReader in = this.getInStream();
+		String inputLine;
 		try {
-			PrintWriter out
-					= new PrintWriter(clientSocket.getOutputStream(), true);
-			BufferedReader in = new BufferedReader(
-					new InputStreamReader(clientSocket.getInputStream()));
-			String inputLine;
 			while ((inputLine = in.readLine()) != null) {
-				System.out.println("Client sent: " + inputLine);
+				System.out.println("Client " + this.threadId + " sent: " + inputLine);
 				out.println(inputLine);
 			}
 		} catch (IOException e) {
-			System.out.println("Failed printing client message ");
+			System.out.println(e.getMessage());
 		}
+		this.closeConnection();
+	}
+	
+	public void closeConnection() {
+		System.out.println("Client connection lost");
+		this.server.removeConnection(threadId);
 	}
 }
