d82c99f8b58c5faadc7cefa8385377fd7cfa8ed0
deanrobertcook <deanrobertcook@gmail.com>
2015-02-28 20:19:31 +0100

    Completed basic implementation of HashTable

--- src/datastructures/HashTable.java
+++ src/datastructures/HashTable.java
@@ -1,47 +1,80 @@
 package datastructures;
 
+import java.util.Objects;
 import utility.Util;
 
 /**
  *
  * @author dean
  */
-public class HashTable {
-	
-	private String[] backingArray;
-	
+public class HashTable<K, E> {
+
+	private LinkedList<KeyValuePair<K, E>>[] backingArray;
+
 	public HashTable() {
 		this(10);
 	}
-	
+
 	public HashTable(int initialSize) {
-		this.backingArray = new String[initialSize];
-	}
-	
-	public void insert(String index, String value) {
-		int hash = this.sumCharValueHash(index);
-		//no collision
-		if (this.backingArray[hash] == null) {
-			this.backingArray[hash] = value;
-		} else {
-			
+		this.backingArray = new LinkedList[initialSize];
+		for (int i = 0; i < backingArray.length; i++) {
+			backingArray[i] = new LinkedList<>();
 		}
 	}
-	
-	private int sumCharValueHash(String index) {
-		char[] indexChars = index.toCharArray();
+
+	public void insert(K key, E value) {
+		int hash = this.hash(key);
+		KeyValuePair<K, E> keyValuePair = new KeyValuePair<>(key, value);
+		this.backingArray[hash].insert(keyValuePair);
+	}
+
+	private int hash(K key) {
+		if (key instanceof String) {
+			return this.sumCharValueHash((String) key);
+		}
+		return -1;
+	}
+
+	private int sumCharValueHash(String key) {
+		char[] indexChars = key.toCharArray();
 		int hash = 0;
 		for (int i = 0; i < indexChars.length; i++) {
-			hash += (int)indexChars[i];
+			hash += (int) indexChars[i];
 		}
-		hash = (int) Math.pow((double)hash, (double)2);
+		hash = (int) Math.pow((double) hash, (double) 2);
 		hash = hash % this.backingArray.length;
 		return hash;
 	}
+
+	public E get(K key) {
+		int hash = this.hash(key);
+		KeyValuePair<K, E> searchKey = new KeyValuePair<>(key, null);
+		int listIndex = this.backingArray[hash].search(searchKey);
+		KeyValuePair<K, E> keyValuePair = this.backingArray[hash].get(listIndex);
+		return keyValuePair.value;
+	}
 	
-	public String get(String index) {
-		int hash = this.sumCharValueHash(index);
-		String value = this.backingArray[hash];
-		return value;
+	class KeyValuePair<K, E> {
+		K key;
+		E value;
+
+		public KeyValuePair(K key, E value) {
+			this.key = key;
+			this.value = value;
+		}
+		
+		@Override
+		public boolean equals(Object anObject) {
+			if (this == anObject) {
+				return true;
+			}
+			if (anObject instanceof KeyValuePair) {
+				KeyValuePair otherKeyValuePair = (KeyValuePair) anObject;
+				if (otherKeyValuePair.key == this.key) {
+					return true;
+				}
+			}
+			return false;
+		}
 	}
 }
